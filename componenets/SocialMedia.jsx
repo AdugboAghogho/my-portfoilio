62696cdf1ffc18ba950c5c80f40b44c01ffc5c33
adugbo aghogho <adugboaghogho@gmail.com>
2024-04-26 08:58:45 +0100

    b

--- componenets/SocialMedia.jsx
+++ componenets/SocialMedia.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BsTwitter, BsInstagram } from 'react-icons/bs';
-import { FaFacebookF } from 'react-icons/fa';
+import { FaFacebookF, FaWhatsappSquare } from 'react-icons/fa';
 import { RiWhatsappLine } from "react-icons/ri";
 
 const SocialMedia = () => (
   <div className="app__social">
     <div>
-      <RiWhatsappLine />
+      <FaWhatsappSquare />
     </div>
     <div>
       <FaFacebookF />
--- container/Skills/Skills.jsx
+++ container/Skills/Skills.jsx
@@ -24,7 +24,7 @@ const Skills = () => {
       setSkills(data);
     });
 
-    
+
 
 
 
