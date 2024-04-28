1e631703d7798718fc7cd9216e956a465716082e
adugbo aghogho <adugboaghogho@gmail.com>
2024-04-28 11:36:36 +0100

    h

--- container/Header/Header.jsx
+++ container/Header/Header.jsx
@@ -61,7 +61,7 @@ const Header = () => (
       whileInView={scaleVariants.whileInView}
       className="app__header-circles"
     >
-      {[images.flutter, images.redux, images.sass].map((circle, index) => (
+      {[images.flutter, images.react, images.sass].map((circle, index) => (
         <div className="circle-cmp app__flex" key={`circle-${index}`}>
           <Image src={circle} alt="profile_bg" />
         </div>
