5eab4017bd72220ecbda585fe0fdba6abedf59d8
adugbo aghogho <adugboaghogho@gmail.com>
2024-04-29 09:13:57 +0100

    d

--- container/Skills/Skills.jsx
+++ container/Skills/Skills.jsx
@@ -3,9 +3,9 @@ import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import ReactTooltip from 'react-tooltip';
 
-import { AppWrap, MotionWrap } from '../../wrapper';
-import { urlFor, client } from '../../client';
-import './Skills.scss';
+import { AppWrap, MotionWrap } from '../../src/wrapper';
+import { urlFor, client } from '../../src/client';
+import './Skills.css';
 
 const Skills = () => {
   const [experiences, setExperiences] = useState([]);
